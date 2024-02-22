c23e0b592f5d14d81d191e962ff9a8f9a2b35994
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2024-02-22 19:57:20 +0600

    board grid

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+
+const App = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div>
+        <div className="flex">
+          <div className="w-20 h-20 m-1 bg-slate-300"></div>
+   <div className="w-20 h-20 m-1 bg-slate-300"></div>
+    <div className="w-20 h-20 m-1 bg-slate-300"></div>
+    </div>
+        <div className="flex">
+          <div className="w-20 h-20 m-1 bg-slate-300"></div>
+   <div className="w-20 h-20 m-1 bg-slate-300"></div>
+    <div className="w-20 h-20 m-1 bg-slate-300"></div>
+    </div>
+        <div className="flex">
+          <div className="w-20 h-20 m-1 bg-slate-300"></div>
+   <div className="w-20 h-20 m-1 bg-slate-300"></div>
+    <div className="w-20 h-20 m-1 bg-slate-300"></div>
+    </div>
+
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
